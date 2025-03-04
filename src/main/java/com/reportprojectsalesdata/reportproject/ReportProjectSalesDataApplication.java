0a57e4331dab48fce283e9cfd89d66ff421b36be
Michal Skowronek <michal.skowronek99.ms@gmail.com>
2025-03-05 00:23:08 +0100

    Initial commit

---
+++ src/main/java/com/reportprojectsalesdata/reportproject/ReportProjectSalesDataApplication.java
@@ -0,0 +1,13 @@
+package com.reportprojectsalesdata.reportproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportProjectSalesDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportProjectSalesDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reportprojectsalesdata/reportproject/ReportProjectSalesDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reportprojectsalesdata.reportproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportProjectSalesDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
